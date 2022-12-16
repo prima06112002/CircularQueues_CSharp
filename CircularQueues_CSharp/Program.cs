a36ad8d8c8a588d994ef6a1e5ef2183d95aef13c
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-16 17:27:43 +0700

    add if in public void display

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -68,5 +68,27 @@ namespace CircularQueues_CSharp
                     FRONT = FRONT + 1;
             }
         }
+        public void display()
+        {
+            int FRONT_position = FRONT;
+            int REAR_position = REAR;
+            /* Checks if the queue is empty. */
+            if (FRONT == -1)
+            {
+                Console.WriteLine("Queue is empty\n");
+                return;
+            }
+            Console.WriteLine("\nElement is the queue are.............\n");
+            if (FRONT_position <= REAR_position)
+            {
+                /* Traverse the queue till the last element present in an array. */
+                while (FRONT_position <= REAR_position)
+                {
+                    Console.Write(queue_array[FRONT_position] + "  ");
+                    FRONT_position++;
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
