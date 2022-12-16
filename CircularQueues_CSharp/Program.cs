98e0ff938a09a6f8527900c53ddb2dd8e10cc21e
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-16 17:03:18 +0700

    start program

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CircularQueues_CSharp
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+
 }
