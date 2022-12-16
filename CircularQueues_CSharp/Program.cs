ea75cb5d7ca04042cf380d4eb61434aa074582f4
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-16 17:21:29 +0700

    add public void insert

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -13,5 +13,35 @@ namespace CircularQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
+        public void insert (int element)
+        {
+            /* This statement checks for the overlow condition. */
+            if ((FRONT == 0 && REAR == max - 1) ||(FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overlow\n");
+                return;
+            }
+            /* The following statement checks whether the queue is empty.if the
+             * queue is empty the value of the REAR and FRONT variable is set to 0.*/
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* if REAR is at the last position of the array,then the value
+                 * of REAR is set to 0 that corresponds to the first position of the array */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /* if REAR is not at the last position then its value is 
+                     * incremented by one. */
+                    REAR = REAR + 1;
+
+            }
+            /* Once the position of REAR is determined, the element is added at its proper place. */
+            queue_array[REAR] = element;
+        }
     }
 }
